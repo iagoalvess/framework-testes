a8e20b481bc19e735c77168a148b24e477c29f01
Iago da Silva Rodrigues Alves <iago.alves@groupsoftware.com.br>
2026-04-10 10:37:56 -0300

    6. Classes TestLoader e TestRunner

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from framework import TestCase, TestResult, TestSuite
+from framework import TestCase, TestResult, TestSuite, TestLoader, TestRunner
 
 
 class TestStub(TestCase):
@@ -111,6 +111,40 @@ class TestSuiteTest(TestCase):
         assert result.summary() == '3 run, 1 failed, 1 error'
 
 
+class TestLoaderTest(TestCase):
+
+    def test_create_suite(self):
+        loader = TestLoader()
+        suite = loader.make_suite(TestStub)
+        assert len(suite.tests) == 3
+
+    def test_create_suite_of_suites(self):
+        loader = TestLoader()
+        stub_suite = loader.make_suite(TestStub)
+        spy_suite = loader.make_suite(TestSpy)
+
+        suite = TestSuite()
+        suite.add_test(stub_suite)
+        suite.add_test(spy_suite)
+
+        assert len(suite.tests) == 2
+
+    def test_get_multiple_test_case_names(self):
+        loader = TestLoader()
+        names = loader.get_test_case_names(TestStub)
+        assert names == ['test_error', 'test_failure', 'test_success']
+
+    def test_get_no_test_case_names(self):
+
+        class Test(TestCase):
+            def foobar(self):
+                pass
+
+        loader = TestLoader()
+        names = loader.get_test_case_names(Test)
+        assert names == []
+
+
 if __name__ == '__main__':
     result = TestResult()
     suite = TestSuite()
@@ -128,5 +162,9 @@ if __name__ == '__main__':
     suite.add_test(TestSuiteTest('test_suite_success_run'))
     suite.add_test(TestSuiteTest('test_suite_multiple_run'))
 
-    suite.run(result)
-    print(result.summary())
+    loader = TestLoader()
+    loader_suite = loader.make_suite(TestLoaderTest)
+    suite.add_test(loader_suite)
+
+    runner = TestRunner()
+    runner.run(suite)
